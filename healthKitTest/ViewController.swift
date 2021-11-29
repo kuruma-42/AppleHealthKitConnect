9dcfe44e017838ed91dc70d30a7b9142a492e0bf
yongjuncha <perfect_yon@naver.com>
2021-11-29 10:47:48 +0900

    Initial Commit

---
+++ healthKitTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  healthKitTest
+//
+//  Created by Yong Jun Cha on 2021/11/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
